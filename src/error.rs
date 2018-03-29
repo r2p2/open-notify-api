1f04bdb21fe1bb92f957aef66f58d4abb22b75eb
Robert Peters <r2p2.gw@gmail.com>
2018-03-29 20:06:25 +0200

    Refactor stringified error type into enum

--- src/error.rs
+++ src/error.rs
@@ -2,24 +2,20 @@ use reqwest;
 use serde_json;
 
 #[derive(Debug)]
-pub struct OpenNotificationError(String);
+pub enum OpenNotificationError {
+    Network(reqwest::Error),
+    Parsing(serde_json::Error),
+    Data(String),
+}
 
 impl From<serde_json::Error> for OpenNotificationError {
     fn from(e: serde_json::Error) -> OpenNotificationError {
-        use std::error::Error;
-        OpenNotificationError(e.description().to_string())
+        OpenNotificationError::Parsing(e)
     }
 }
 
 impl From<reqwest::Error> for OpenNotificationError {
     fn from(e: reqwest::Error) -> OpenNotificationError {
-        use std::error::Error;
-        OpenNotificationError(e.description().to_string())
-    }
-}
-
-impl From<String> for OpenNotificationError {
-    fn from(s: String) -> OpenNotificationError {
-        OpenNotificationError(s)
+        OpenNotificationError::Network(e)
     }
 }
